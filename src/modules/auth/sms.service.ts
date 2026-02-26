64178ed271cb2c94e0a7103304720d74fe5f9fe4
Mirxonjon <mirxonjonismanov152@gmail.com>
2026-02-26 18:03:54 +0500

    fixed bug

--- src/modules/auth/sms.service.ts
+++ src/modules/auth/sms.service.ts
@@ -8,8 +8,8 @@ export class SmsService {
     const url = 'https://gate.coachingzona.uz/send';
 
     const payload = {
-      phones: ["+998909378788"], // masalan: 998974409931
-      text: `Charge One — Код подтверждения для входа: ${code}. Никому не сообщайте этот код.`,
+      phones: [phone], // masalan: 998974409931
+      text: `Charge One — Код подтверждения: ${code}.`,
     };
 
     try {
